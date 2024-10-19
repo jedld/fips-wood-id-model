344620ccfe4a25d100e93cff4dbab72df4f6beba
Joseph Emmanuel Dayo <joseph.dayo@gmail.com>
2024-10-19 19:22:27 +0800

    Update README related to the mobile app model tool

--- README.md
+++ README.md
@@ -1,2 +1,117 @@
 # fips-wood-id-model-luke-test
 Takes Python model used to identify wood and converts it to a form useable in a mobile app
+
+## Setup
+
+1. Install [Python 3](https://www.python.org/downloads/) (if you don't have it already)
+2. Clone this repo: `git clone git@github.com:jedld/fips-wood-id-model.git`
+3. Install the required packages by running `pip install -r requirements.txt` from inside the project directory
+
+
+### How to generate a model for mobile phones
+
+You can use the `generate_mobile_model.py` script to convert a PyTorch model to a format suitable for mobile applications. The script allows you to specify various parameters such as the model file, class labels, sample input, model name, and description via command-line options.
+
+#### Command-line Options
+
+- `-m`, `--model`: Path to the model weights file (default: `wts.pth`)
+- `-c`, `--class_labels`: Path to the class labels file (default: `class_labels.txt`)
+- `-s`, `--sample_input`: Path to the sample input image file (default: `../imgdb/000.png`)
+- `-n`, `--name`: Name of the model (default: `resnet-18-31-class`)
+- `-d`, `--description`: Description of the model (default: `31 class resnet50 Wood Model V5`)
+- `-t`, `--type`: Type of the model architecture (default: `resnet18`)
+- `-o`, `--output`: Output file name for the generated model archive (default: `model.zip`)
+- `-p`, `--path`: Path to the directory containing the input files (default: `.`)
+
+#### Examples
+
+1. Generate a mobile model with default settings:
+    ```sh
+    python generate_mobile_model.py
+    ```
+
+2. Generate a mobile model with a specific model file and class labels:
+    ```sh
+    python generate_mobile_model.py -m my_model.pth -c my_class_labels.txt
+    ```
+
+3. Generate a mobile model with a custom name and description:
+    ```sh
+    python generate_mobile_model.py -n "custom-model" -d "Custom Model Description"
+    ```
+
+4. Generate a mobile model and specify the output file name:
+    ```sh
+    python generate_mobile_model.py -o custom_model.zip
+    ```
+
+5. Generate a mobile model with a specific sample input image:
+    ```sh
+    python generate_mobile_model.py -s ../imgdb/sample_image.png
+    ```
+
+6. Generate a mobile model with a specific model architecture:
+    ```sh
+    python generate_mobile_model.py -t resnet50
+    ```
+
+7. Generate a mobile model with all custom parameters:
+    ```sh
+    python generate_mobile_model.py -m my_model.pth -c my_class_labels.txt -s ../imgdb/sample_image.png -n "custom-model" -d "Custom Model Description" -t resnet50 -o custom_model.zip -p /path/to/files
+    ```
+
+This script will generate a zip file containing the optimized model and related metadata, which can be used in mobile applications.
+
+## Transfering the zip file to the device (Android):
+
+Preqrequisites:
+
+- The PhilWoodID or varients thereof is installed in the Android Device.
+
+### Android
+
+#### 1. Transfering the Zip file via Email or 3rd party tools:
+
+- Send the zip file to the email address of the user.
+- Use a 3rd party tool like [Dropbox](https://www.dropbox.com/) to transfer the zip file to the device.
+- Make sure to save the Zip file in a location that can be accessed by other apps on the device. For example, if you are using Dropbox, you should save it under the "Dropbox" folder. Or if via Gmail, you should save it under the "Google Drive" folder.
+- If the device is connected via USB, you may also be able to transfer the zip file directly from the PC to the device.
+
+Note: Do NOT extract the zip file. The zip file contains all the files required for running the model on the device and the mobile app will be the one that extracts them automatically.
+
+#### 2. Loading the Zip file using the PhilWoodID app:
+
+- Open the PhilWoodID app on your Android Device.
+- Once on the PhilWoodID main screen, click on the tertiary button (3 dots) located at the top right corner of the screen.
+- Tap on the "Updates...". This will open a new screen with the option to "Install a Model". Click on this and a new screen will show up to select the zip file from where you saved.
+- After selecting the zip file, the app will load and extract the model immediately in the background, however it will not be active.
+- To activate the model, click on the "Activate" button associated to the model you just loaded. Note that your new model should show up in the list alongside other previously installed models.
+
+
+## IOS
+
+#### Transfering the zip file to the device (iOS):
+
+Various methods are available for transferring the zip file to the device. The following are some of them:
+
+- 1. Send the zip file via email or 3rd party tools like [Dropbox](https://www.dropbox.com/) and [Google Drive](https://drive.google.com/).
+- 2. Use AirDrop to transfer the zip file from your PC to the device.
+- 3. Connect the device via USB, then use Finder or iTunes to transfer the zip file directly to the device.
+
+Depending on how you transfered the zip file to the device, please note the location on where it was saved. Do not extract the zip file.
+
+#### 2. Loading the Zip file using the PhilWoodID app:
+
+Preqrequisites:
+- The PhilWoodID or varients thereof is installed in the iOS Device.
+
+
+1. Open the PhilWoodID app on your iOS Device.
+2. Once on the PhilWoodID main screen, click on the Settings (Cog Wheel Icon) button at the top right of the screen.
+3. The Settings Screen will show up but navigate to the bottom and click on the "Update Model" button. This will open the IOS screen to select the zip file from where you transferred it earlier.
+4. Select the zip file that was transferred to your iOS Device.
+5. Wait for the zip file to be processed by the app and loaded onto the device. Once done, the PhilWoodID app will show a message indicating that the model has been updated successfully. The selected model will be active on the device.
+
+
+
+
--- model/generate_mobile_model.py
+++ model/generate_mobile_model.py
@@ -3,9 +3,6 @@ from image_resnet_transfer_classifier import ImageResNetTransferClassifier
 from torchvision.transforms import transforms
 from torch.utils.mobile_optimizer import optimize_for_mobile
 from PIL import Image
-from torch.autograd import Variable
-import data
-import torch.nn as nn
 import model
 import image_augmentations as ia
 import csv
@@ -13,14 +10,49 @@ import json
 import zipfile
 import datetime
 import os
+from pathlib import Path
+from optparse import OptionParser
 
+# Command-line options
+parser = OptionParser()
+parser.add_option("-m", "--model", dest="model_file", default="wts.pth",
+                  help="path to the model weights file", metavar="FILE")
+parser.add_option("-c", "--class_labels", dest="class_labels_file", default="class_labels.txt",
+                  help="path to the class labels file", metavar="FILE")
+parser.add_option("-s", "--sample_input", dest="sample_input_file", default="../imgdb/000.png",
+                  help="path to the sample input image file", metavar="FILE")
+parser.add_option("-n", "--name", dest="model_name", default="resnet-18-31-class",
+                  help="name of the model", metavar="NAME")
+parser.add_option("-d", "--description", dest="model_description", default="31 class resnet50 Wood Model V5",
+                  help="description of the model", metavar="DESCRIPTION")
+parser.add_option("-t", "--type", dest="model_type", default="resnet18")
+parser.add_option("-o", "--output", dest="output", default="model.zip")
+parser.add_option("-p","--path", dest="path", default=".")
 
+(options, args) = parser.parse_args()
 
+# define and load the model here
+def load_model(class_labels_file_path, model_file_path, arch='resnet18'):
+    with open(class_labels_file_path, 'r') as fh:
+      lines = [line.strip() for line in fh.readlines()]
+    cls = ImageResNetTransferClassifier(body_arch=arch, num_classes=len(lines))
+    cls.load_weights(Path(model_file_path))
 
-classifier, classnames = model.get('class_labels.txt','wts2.pth')
+    return cls, lines
+
+x = datetime.datetime.now()
+model_version = x.strftime('%Y%m%d%H%M%S')
+
+# Load model and class
+class_labels_file_path = os.path.join(options.path, options.class_labels_file)
+model_file_path = os.path.join(options.path, options.model_file)
+sample_input_file_path = os.path.join(options.path, options.sample_input_file)
+
+classifier, classnames = load_model(class_labels_file_path, model_file_path, arch=options.model_type)
 classifier.eval()
 
-image = Image.open('../imgdb/Test/Acacia auriculiformis/Auri.5x.FPRDI.Authentic (1-1).jpg')
+# Load sample input image
+image = Image.open(sample_input_file_path)
 
 divfac = 4
 resize_size = (2048//divfac, 2048//divfac)
@@ -34,12 +66,43 @@ sample = xfm(sample)
 input = sample['image'][0].unsqueeze(0)
 print("input dimension", input.shape)
 
+# Convert model to TorchScript and optimize for mobile
 torchscript_model = torch.jit.script(classifier)
 torchscript_model = optimize_for_mobile(torchscript_model)
 print("generating mobile model pytorch version:", torch.__version__)
-torch.jit.save(torchscript_model, "fips_wood_model_mobile.pt")
+torch.jit.save(torchscript_model, "tmp_fips_wood_model_mobile.pt")
 
-import csv
+# Load species database
+database_content = {}
+
+specied_database_file = os.path.join(options.path, 'species_database.csv')
+species_info_json_file = os.path.join(options.path, 'species_database.json')
+
+if not os.path.exists(specied_database_file):
+  print("species_database.csv not found will generate a sample template. Until this is properly filled up labels in the mobile app will be equal to the class label which may not be user friendly.\n It is recommended to edit the csv and rerun this script.")
+
+  with open(specied_database_file, 'w') as csv_file:
+    csv_file.write("scientific_name,other_names\n")
+    for class_name in classnames:
+      csv_file.write(f"{class_name},{class_name}\n")
+
+  print(f"species_database.csv generated and placed at {specied_database_file}. Please edit the file and rerun this script.")
+
+if not os.path.exists(species_info_json_file):
+  print(f"species_database.json not found will generate a sample template. Until this is properly filled up labels in the mobile app will be equal to the class label which may not be user friendly.\n It is recommended to edit the json and rerun this script.")
+
+  with open(species_info_json_file, 'w') as json_file:
+    json.dump({}, json_file)
+
+  print(f"species_database.json generated and placed at {species_info_json_file}. Please edit the file and rerun this script.")
+
+model_txt_file = os.path.join(options.path, 'model.txt')
+
+if not os.path.exists(model_txt_file):
+  print("Generating model.txt place version string here and rerun this script.")
+  with open(model_txt_file, 'w') as txt_file:
+    current_date = datetime.datetime.now()
+    txt_file.write(f"{current_date.strftime('%Y%m%d%H%M%S')}")
 
 database_content = {}
 with open('species_database.csv') as csv_file:
@@ -58,34 +121,27 @@ with open('species_database.csv') as csv_file:
           }
   print(f'Processed {line_count} lines.')
 
-
-
-print("Building mobile asset archive...")
-
-x = datetime.datetime.now()
-model_version = x.strftime('%Y%m%d%H%M%S')
 model_info = {
-  'name' : 'resnet-18-31-class',
-  'description' : '31 class resnet50 Wood Model V5',
+  'name' : options.model_name,
+  'description' : options.model_description,
   'version' : model_version,
   'pytorch' : torch.__version__,
   'input_dimension' : input.shape,
-  'accuracy_cutoff' : 4.0
+  'accuracy_cutoff' : 0.6
 }
 
 with open('model.json', 'w') as outfile:
   json.dump(model_info, outfile, indent=2)
 
-zipf = zipfile.ZipFile(f'model-{model_version}.zip', 'w', zipfile.ZIP_DEFLATED)
-zipf.write('species_database.json', 'species_database.json')
-zipf.write('fips_wood_model_mobile.pt', 'model.pt')
-zipf.write('model.txt','model.txt')
-zipf.write('model.json','model.json')
-zipf.write('class_labels.txt','labels.txt')
-
 reference_images = []
 print(database_content.keys())
-for subdir, dirs, files in os.walk('reference'):
+if not os.path.exists('reference'):
+  print("reference directory not found. Please place the reference images in the reference directory and rerun this script.")
+  os.makedirs('reference')
+  for class_name in database_content.keys():
+    os.makedirs(f'reference/{class_name}', exist_ok=True)
+
+for subdir, _, files in os.walk('reference'):
   for file in files:
     #print os.path.join(subdir, file)
     filepath = subdir + os.sep + file
@@ -95,18 +151,26 @@ for subdir, dirs, files in os.walk('reference'):
       print(f"{class_name} is not in the species_database.csv file!")
     else:
       content_detail = database_content[class_name]
-
       if 'reference_images' not in content_detail:
         content_detail['reference_images'] = []
-
       content_detail['reference_images'].append(filepath.replace(' ', '_'))
 
 with open('species_database.json', 'w') as outfile:
   json.dump(database_content, outfile, indent=2)
+output_file = os.path.join(options.path, options.output)
+zipf = zipfile.ZipFile(output_file, 'w', zipfile.ZIP_DEFLATED)
+zipf.write('species_database.json', 'species_database.json')
+zipf.write("tmp_fips_wood_model_mobile.pt", 'model.pt')
+zipf.write('model.txt','model.txt')
+zipf.write('model.json','model.json')
+zipf.write('class_labels.txt','labels.txt')
 
 for filepath, transformed_filepath in reference_images:
   zipf.write(filepath, transformed_filepath)
 
 zipf.close()
 
-print(f"model written to model-{model_version}.zip")
+# delete temp files
+os.remove("tmp_fips_wood_model_mobile.pt")
+
+print(f"model written to {output_file}")
